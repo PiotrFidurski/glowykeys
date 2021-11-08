fa0d6ffb1bfbe2112f66dfcc17ee9c45bcba082d
Piotr Fidurski <crunor55@hotmail.com>
2021-11-08 21:39:39 +0100

    Delete HomePage.test.tsx

--- cypress/e2e/ui/HomePage.test.tsx
+++
@@ -1,9 +0,0 @@
-describe('HomePage tests', () => {
-  it('renders hello', () => {
-    cy.visit('/');
-    cy.findByText(/hello/i);
-  });
-});
-
-// eslint-disable-next-line jest/no-export
-export {};
