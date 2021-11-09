bb6d5c6e0224a1a1e362e57a47d68ba47aa46c72
Chimson <crunor55@hotmail.com>
2021-11-09 12:33:25 +0100

    add Head and meta title tags to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
+import Navbar from '@components/Navbar/Navbar';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
-const HomePage: NextPage = () => <div>hello</div>;
+const HomePage: NextPage = () => (
+  <div>
+    <Head>
+      <title>Find the best keyboards</title>
+      <meta name="description" content="shop for the best keyboards around" />
+    </Head>
+    <Navbar />
+    hello
+  </div>
+);
 
 export default HomePage;
