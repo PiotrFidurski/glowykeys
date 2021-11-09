de1a7b2e422a27f160494f0b4f385902c20215e6
Chimson <crunor55@hotmail.com>
2021-11-09 12:32:47 +0100

    use normalize and global styles in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import { GlobalStyles } from '@styled/GlobalStyles';
+import { theme } from '@styled/theme';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Normalize />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
