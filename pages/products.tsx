07ff8210304c245d66fd7c1533b0286c4d3de1c4
Chimson <crunor55@hotmail.com>
2021-11-12 16:13:14 +0100

    add products page

---
+++ pages/products.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@components/Navbar/Navbar';
+import * as React from 'react';
+
+function ProductsPage() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default ProductsPage;
