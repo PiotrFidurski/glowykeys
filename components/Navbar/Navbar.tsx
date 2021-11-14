6da7d388c88caf4769fd4c373daa1dc5c639097d
Chimson <crunor55@hotmail.com>
2021-11-14 18:03:15 +0100

    add useCart to Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useCart } from '@components/Cart/useCart';
 import Link from 'next/link';
 import * as React from 'react';
 import Cart from '../../public/assets/vector/cart.svg';
@@ -5,6 +6,7 @@ import LogoIcon from '../../public/assets/vector/logo.svg';
 import { Button, Li, Logo, LogoSectionList, Nav, SignInLink, Ul } from './styles';
 
 function Navbar() {
+  const { setOpen } = useCart();
   return (
     <Nav>
       <LogoSectionList>
@@ -24,7 +26,7 @@ function Navbar() {
           </Link>
         </Li>
         <Li>
-          <Button type="button">
+          <Button onClick={() => setOpen(true)} type="button" aria-label="open cart menu">
             <Cart fill="white" width="25" height="25" />
           </Button>
         </Li>
