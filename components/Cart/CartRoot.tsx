cd24121f0130dc33933a1cb4c7056b8d4771890b
Chimson <crunor55@hotmail.com>
2021-11-15 18:33:46 +0100

    add focus restoration and focus active element for cart menu open/close states

--- components/Cart/CartRoot.tsx
+++ components/Cart/CartRoot.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import * as React from 'react';
 import { createPortal } from 'react-dom';
 import { CardBackdrop, CartContainer } from './styles';
@@ -11,20 +12,48 @@ function CartRoot({ cartUi }: Props) {
 
   const cartContainer = React.useRef<null | HTMLDivElement>(null);
 
+  const [mounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  React.useEffect(() => {
+    const currentElement = document.activeElement as HTMLElement;
+
+    if (open) cartContainer.current.focus();
+
+    return () => {
+      currentElement.focus();
+    };
+  }, [open]);
+
   const closeCart = (e: React.BaseSyntheticEvent) => {
     if (!cartContainer.current.contains(e.target)) {
       setOpen(false);
     }
   };
 
-  return open
+  return mounted
     ? createPortal(
-        <CardBackdrop aria-label="Close cart" onClick={closeCart}>
-          <CartContainer tabIndex={0} ref={cartContainer} aria-label="cart">
-            {cartUi}
-          </CartContainer>
-        </CardBackdrop>,
-        document.getElementById('portal') || null
+        <AnimatePresence>
+          {open ? (
+            <CardBackdrop aria-label="Close cart" onClick={closeCart}>
+              <CartContainer
+                as={motion.div}
+                initial={{ opacity: 0, x: 1000, scale: 0.3 }}
+                animate={{ opacity: 1, x: 0, scale: 1 }}
+                exit={{ opacity: 0, x: 200, scale: 0.5 }}
+                tabIndex={0}
+                ref={cartContainer}
+                aria-label="cart"
+              >
+                {cartUi}
+              </CartContainer>
+            </CardBackdrop>
+          ) : null}
+        </AnimatePresence>,
+        document.getElementById('portal') || document.createElement('div')
       )
     : null;
 }
