07ace280df47280ca4e3a8562e5812da81bbcbf4
Chimson <crunor55@hotmail.com>
2021-11-15 20:40:40 +0100

    replace useState with useReducer

--- components/Cart/CartProvider.tsx
+++ components/Cart/CartProvider.tsx
@@ -1,16 +1,25 @@
+import { cartData } from 'data';
 import * as React from 'react';
 import { CartContext } from './CartContext';
 import CartRoot from './CartRoot';
+import { Action, State } from './types';
 
 interface Props {
   ui: React.ReactElement;
+  reducer: (state: State, action: Action) => State;
 }
 
-function CartProvider({ children, ui }: React.PropsWithChildren<Props>) {
-  const [open, setOpen] = React.useState(false);
+const initialState: State = {
+  open: false,
+  items: cartData,
+};
 
+function CartProvider({ children, ui, reducer }: React.PropsWithChildren<Props>) {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const contextValue = React.useMemo(() => ({ state, dispatch }), [state]);
   return (
-    <CartContext.Provider value={{ open, setOpen }}>
+    <CartContext.Provider value={contextValue}>
       {children}
       <CartRoot cartUi={ui} />
     </CartContext.Provider>
