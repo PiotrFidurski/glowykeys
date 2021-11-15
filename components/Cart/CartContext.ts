757529a28570980c8ade11285f5254f5b9787826
Chimson <crunor55@hotmail.com>
2021-11-15 20:40:19 +0100

    add context module functions

--- components/Cart/CartContext.ts
+++ components/Cart/CartContext.ts
@@ -1,8 +1,27 @@
 import * as React from 'react';
+import { Action, actionTypes, State, UpdateItemQtyProps } from './types';
 
 interface CartContextProps {
-  open: boolean;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  state: State;
+  dispatch: React.Dispatch<Action>;
+}
+
+export function incrementQty({ dispatch, updates }: UpdateItemQtyProps) {
+  const { items, product } = updates;
+  const index = items.findIndex((item) => item.id === product.id);
+  const newItem = { ...product, qty: product.qty + 1 };
+  const itemsCopy = [...items];
+  itemsCopy[index] = newItem;
+  dispatch({ type: actionTypes.incrementQty, payload: itemsCopy });
+}
+
+export function decrementQty({ dispatch, updates }: UpdateItemQtyProps) {
+  const { items, product } = updates;
+  const index = items.findIndex((item) => item.id === product.id);
+  const newItem = { ...product, qty: product.qty - 1 };
+  const itemsCopy = [...items];
+  itemsCopy[index] = newItem;
+  dispatch({ type: actionTypes.incrementQty, payload: itemsCopy });
 }
 
 const CartContext = React.createContext<CartContextProps | null>(null);
