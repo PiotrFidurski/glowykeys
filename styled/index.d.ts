77767fe0fa95273498d39084259e5b94534577f9
Chimson <crunor55@hotmail.com>
2021-11-19 19:09:14 +0100

    add new theme options

--- styled/index.d.ts
+++ styled/index.d.ts
@@ -2,9 +2,18 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
+    boxShadowColor: {
+      primary: string;
+    };
+    border: {
+      color: {
+        primary: string;
+      };
+    };
     background: {
       primary: string;
       secondary: string;
+      card: string;
     };
     color: {
       primary: string;
--- styled/theme.ts
+++ styled/theme.ts
@@ -1,9 +1,16 @@
 import { DefaultTheme } from 'styled-components';
 
 const theme: DefaultTheme = {
+  boxShadowColor: { primary: 'rgb(255 255 255 / 5%)' },
+  border: {
+    color: {
+      primary: 'rgb(255 255 255 / 5%)',
+    },
+  },
   background: {
     primary: '#030408',
     secondary: '#000000',
+    card: '#090c13',
   },
   color: {
     primary: '#ffffff',
