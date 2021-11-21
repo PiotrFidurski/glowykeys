db012c92eeada7236054b6b5f51cdd9dc0e6be8b
Chimson <crunor55@hotmail.com>
2021-11-21 15:52:34 +0100

    add KeyboardsPage tests

---
+++ __tests__/KeyboardsPage.test.tsx
@@ -0,0 +1,51 @@
+import KeyboardsPage from '@pages/keyboards';
+import { screen } from '@testing-library/react';
+import { render } from '@utils/test-utils';
+import { data } from 'data';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '',
+      pathname: '',
+      query: '',
+      asPath: '',
+    };
+  },
+}));
+
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+
+test('when queryParams are "?variants=gaming" it renders only filtered results', () => {
+  useRouter.mockImplementation(() => ({
+    route: '/keyboards',
+    pathname: '/keyboards',
+    query: { variants: ['gaming'] },
+    asPath: '',
+    push: jest.fn(),
+  }));
+
+  render(<KeyboardsPage keyboards={data} />);
+
+  const productList = screen.getByRole('region', { name: /list of products/i });
+
+  expect(productList.children).toHaveLength(3);
+});
+
+test('when queryParams are "?connectivity=wired" it renders only filtered results', () => {
+  useRouter.mockImplementation(() => ({
+    route: '/keyboards',
+    pathname: '/keyboards',
+    query: { connectivity: ['wired'] },
+    asPath: '',
+    push: jest.fn(),
+  }));
+
+  render(<KeyboardsPage keyboards={data} />);
+
+  const productList = screen.getByRole('region', { name: /list of products/i });
+
+  expect(productList.children).toHaveLength(3);
+
+  jest.restoreAllMocks();
+});
