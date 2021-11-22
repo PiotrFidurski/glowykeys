f9974d7bcbfcb6edd855744824f66def867e3902
Chimson <crunor55@hotmail.com>
2021-11-22 17:27:57 +0100

    add keycaps api route, build keycaps page

--- pages/keycaps.tsx
+++ pages/keycaps.tsx
@@ -1,7 +1,80 @@
+import AppliedFilters from '@components/AppliedFilters/AppliedFilters';
+import Filters from '@components/Filters/Filters';
+import { FilterTypes, SortTypes } from '@components/Filters/types';
+import Footer from '@components/Footer/Footer';
+import Navbar from '@components/Navbar/Navbar';
+import Product from '@components/Product/Product';
+import Sorts from '@components/Sorts/Sorts';
+import { FilterSection, H1, Header, Main, ProductsContainer, ProductsSection } from '@styled/pages/KeyboardsPageStyles';
+import { compare } from '@utils/compare';
+import { possibleFilters } from '@utils/filters';
+import { VisuallyHiddenH2 } from '@utils/style-utils';
+import { Product as ProductType } from '@utils/types';
+import { AnimateSharedLayout } from 'framer-motion';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 
-function KeycapsPage() {
-  return <div>hello keycaps</div>;
+interface Props {
+  keycaps: Array<ProductType>;
 }
+
+function KeycapsPage({ keycaps }: Props) {
+  const { query } = useRouter();
+
+  const [sort, setSort] = React.useState<SortTypes>({ order: 'ASC', type: 'price' });
+
+  const [filters, setFilters] = React.useState<FilterTypes>(() => ({
+    brands: query.brands ? [].concat(query.brands) : [],
+  }));
+
+  return (
+    <>
+      <Head>
+        <title>Browse for Keycaps</title>
+        <meta name="description" content="Find all kinds of unique looking keycaps" />
+      </Head>
+      <Navbar />
+      <Main>
+        <Header>
+          <Image src="/assets/images/keycapsbanner.png" priority layout="fill" objectFit="cover" />
+          <H1>Pick the most unique looking keycaps for your keyboards.</H1>
+        </Header>
+        <AppliedFilters currentFilters={filters} setFilters={setFilters} />
+        <ProductsSection aria-labelledby="products-section" role="region">
+          <VisuallyHiddenH2 id="products-section">Products section</VisuallyHiddenH2>
+          <AnimateSharedLayout>
+            <FilterSection aria-label="filter products menu" role="region">
+              <VisuallyHiddenH2>Product filters</VisuallyHiddenH2>
+              <Filters filters={filters} filtersFor={{ brands: ['M7', 'KBDfans'] }} onFilter={setFilters} />
+              <Sorts onSort={setSort} sort={sort} />
+            </FilterSection>
+            <ProductsContainer role="region" aria-label="list of products">
+              <VisuallyHiddenH2>Product list</VisuallyHiddenH2>
+              {keycaps
+                .filter((product) => possibleFilters.every((filterFn) => filterFn({ product, ...filters })))
+                .sort((productA, productB) => compare({ productA, productB, ...sort }))
+                .map((product) => (
+                  <Product product={product} key={product.id} />
+                ))}
+            </ProductsContainer>
+          </AnimateSharedLayout>
+        </ProductsSection>
+        <Footer />
+      </Main>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/api/keycaps');
+  const { data }: { data: Array<ProductType> } = await response.json();
+
+  return {
+    props: { keycaps: data },
+  };
+};
 
 export default KeycapsPage;
