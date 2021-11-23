962994f2b34536f5964d388990c51e705c4c1be3
Chimson <crunor55@hotmail.com>
2021-11-23 15:19:49 +0100

    fix: remove headers from one page

--- pages/keycaps.tsx
+++ pages/keycaps.tsx
@@ -75,8 +75,6 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 
   const { data }: { data: Array<ProductType> } = await response.json();
 
-  res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59');
-
   return {
     props: { keycaps: data },
   };
