06ef168144358470a9bf7225f374b0d5c1542d41
Chimson <crunor55@hotmail.com>
2021-11-25 15:23:56 +0100

    chore: increase border size

--- lib/style-utils.ts
+++ lib/style-utils.ts
@@ -61,7 +61,7 @@ export const SquareButton = styled.button`
   max-width: 300px;
   width: 100%;
   color: ${({ theme: { color } }) => color.primary};
-  border: 2px solid ${({ theme: { color } }) => color.highlight};
+  border: 3px solid ${({ theme: { color } }) => color.highlight};
   min-width: 200px;
   transition: border-color 0.3s linear;
 
@@ -100,7 +100,7 @@ export const RoundButton = styled.button<{ transformOn?: boolean }>`
   transform: ${({ transformOn }) => (transformOn ? 'rotate(180deg)' : 'rotate(0)')};
 
   &:focus {
-    outline: 2px solid ${({ theme: { color }, transformOn }) => (transformOn ? color.danger : color.highlight)};
+    outline: 3px solid ${({ theme: { color }, transformOn }) => (transformOn ? color.danger : color.highlight)};
     background: ${({ theme: { color } }) => color.accent};
   }
 
@@ -114,7 +114,7 @@ export const RoundButton = styled.button<{ transformOn?: boolean }>`
 export const SmallButton = styled.button`
   ${ButtonStyles};
   border-radius: 9999px;
-  border: 2px solid ${({ theme: { color } }) => color.highlight};
+  border: 3px solid ${({ theme: { color } }) => color.highlight};
   font-size: 1rem;
   gap: 1rem;
   color: ${({ theme: { color } }) => color.highlight};
@@ -128,7 +128,7 @@ export const SmallButton = styled.button`
 
   &:hover {
     color: ${({ theme: { color } }) => color.danger};
-    border: 2px solid ${({ theme: { color } }) => color.danger};
+    border: 3px solid ${({ theme: { color } }) => color.danger};
     svg {
       fill: ${({ theme: { color } }) => color.danger};
     }
