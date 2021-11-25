f10397cd0fa81dd26c5a301a4f287a6b2b3e68ed
Chimson <crunor55@hotmail.com>
2021-11-25 19:03:42 +0100

    refactor: add more style to footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,22 +1,55 @@
+import { theme } from '@styled/theme';
+import { VisuallyHiddenH2 } from '@utils/style-utils';
 import * as React from 'react';
-import { A, About, Help, StyledFooter } from './styles';
+import InstagramIcon from '../../public/assets/vector/instagram.svg';
+import TwitterIcon from '../../public/assets/vector/twitter.svg';
+import YoutubeIcon from '../../public/assets/vector/youtube.svg';
+import * as S from './styles';
 
 function Footer() {
   return (
-    <StyledFooter>
-      <Help>
-        <A>Frequently Asked Questions</A>
-        <A>Reviews</A>
-        <A>My Account</A>
-        <A>Contact Us</A>
-      </Help>
-      <About>
-        <A>Press</A>
-        <A>Our Story</A>
-        <A>Products</A>
-        <A>Technology</A>
-      </About>
-    </StyledFooter>
+    <S.StyledFooter>
+      <S.FooterWrapper>
+        <S.ContactUs>
+          <h2>Contact Us</h2>
+          <p>
+            Here at Glowykeys it&apos;s all about keyboards and keyboard things. We share the same passion that you do,
+            help us find the product for your needs.
+          </p>
+          <S.IconsContainer>
+            <InstagramIcon fill={theme.color.primary} width="40" height="40" />
+            <YoutubeIcon fill={theme.color.primary} width="40" height="40" />
+            <TwitterIcon fill={theme.color.primary} width="40" height="40" />
+          </S.IconsContainer>
+          <S.ContactFormContainer>
+            <input type="text" />
+            <S.Button>
+              <span>contact</span>
+            </S.Button>
+          </S.ContactFormContainer>
+        </S.ContactUs>
+        <S.Help>
+          <VisuallyHiddenH2>Help</VisuallyHiddenH2>
+          <S.Nav role="navigation" aria-label="customer service">
+            <h2>Customer Service</h2>
+            <S.A href="/">Frequently Asked Questions</S.A>
+            <S.A href="/">Reviews</S.A>
+            <S.A href="/">My Account</S.A>
+            <S.A href="/">Contact Us</S.A>
+          </S.Nav>
+        </S.Help>
+        <S.About>
+          <VisuallyHiddenH2>Other Links</VisuallyHiddenH2>
+          <S.Nav>
+            <h2>About</h2>
+            <S.A href="/">Press</S.A>
+            <S.A href="/">Our Story</S.A>
+            <S.A href="/">Products</S.A>
+            <S.A href="/">Technology</S.A>
+          </S.Nav>
+        </S.About>
+      </S.FooterWrapper>
+    </S.StyledFooter>
   );
 }
 
--- components/Footer/styles.ts
+++ components/Footer/styles.ts
@@ -2,43 +2,129 @@ import { LineOnHover } from '@utils/style-utils';
 import styled from 'styled-components';
 
 export const StyledFooter = styled.footer`
-  grid-area: footer;
-  padding: 1rem 2rem;
   background: ${({ theme: { background } }) => background.secondary};
+  width: 100%;
+  grid-area: footer;
+`;
+
+export const FooterWrapper = styled.div`
+  padding: 1rem 3rem;
+  min-height: 500px;
+  max-width: 1700px;
+  margin: 0 auto;
   display: grid;
-  grid-template-columns: repeat(4, minmax(auto, 1fr));
+  grid-template-columns: repeat(6, minmax(auto, 1fr));
   grid-template-areas:
-    'help help help help'
-    'about about about about';
+    'contact-us contact-us contact-us contact-us contact-us contact-us'
+    'help help help help help help'
+    'about about about about about about';
 
   @media (min-width: 768px) {
-    grid-template-areas: 'help help about about';
+    grid-template-areas: 'contact-us contact-us help help about about';
+  }
+`;
+
+export const ContactUs = styled.section`
+  grid-area: contact-us;
+  max-width: 455px;
+
+  h2 {
+    color: ${({ theme: { color } }) => color.highlight};
+  }
+
+  p {
+    line-height: 1.525rem;
+  }
+`;
+
+export const Nav = styled.nav`
+  flex-direction: column;
+  display: flex;
+  gap: 1rem;
+`;
+
+export const IconsContainer = styled.div`
+  display: flex;
+  padding: 2rem 0;
+  align-items: center;
+  gap: 2rem;
+
+  svg {
+    transition: fill 0.3s ease;
+    &:hover {
+      cursor: pointer;
+      fill: ${({ theme: { color } }) => color.highlight};
+    }
+  }
+`;
+
+export const ContactFormContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  gap: 0rem;
+  border: 3px solid ${({ theme: { color } }) => color.highlight};
+  justify-content: stretch;
+  min-height: 50px;
+  background: ${({ theme: { color } }) => color.highlight};
+  margin-bottom: 2rem;
+
+  input {
+    background: ${({ theme: { background } }) => background.secondary};
+    border: 0;
+    padding: 0 1rem;
+    outline-color: ${({ theme: { color } }) => color.highlight};
+    color: ${({ theme: { color } }) => color.primary};
+    min-height: 50px;
+    max-width: 350px;
+    width: 100%;
   }
 `;
 
 export const Help = styled.section`
   grid-area: help;
-  display: flex;
-  gap: 1rem;
-  margin-bottom: 3rem;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+
+  h2 {
+    color: ${({ theme: { color } }) => color.highlight};
+  }
 
   @media (min-width: 768px) {
-    margin-bottom: 0;
+    margin-left: 3rem;
   }
 `;
 
 export const About = styled.section`
   grid-area: about;
-  display: flex;
-  gap: 1rem;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+
+  h2 {
+    color: ${({ theme: { color } }) => color.highlight};
+  }
+
+  @media (min-width: 768px) {
+    margin-left: 3rem;
+  }
 `;
 
 export const A = styled.a`
   ${LineOnHover}
+  width: fit-content;
+  text-decoration: none;
+  color: ${({ theme: { color } }) => color.primary};
+`;
+
+export const Button = styled.button`
+  padding: 1rem;
+  background: ${({ theme: { color } }) => color.highlight};
+  color: ${({ theme: { color } }) => color.accent};
+  border: 1px solid ${({ theme: { color } }) => color.highlight};
+  border-left: 0;
+  color: ${({ theme: { background } }) => background.secondary};
+  text-transform: uppercase;
+  text-align: center;
+  font-weight: 600;
+  flex-shrink: 1;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
