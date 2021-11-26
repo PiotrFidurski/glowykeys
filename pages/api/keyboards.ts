7256a84a58f0730e766b273df50b32f71d728055
Chimson <crunor55@hotmail.com>
2021-11-26 18:33:05 +0100

    feat: add placeholder images based on current image

--- pages/api/keyboards.ts
+++ pages/api/keyboards.ts
@@ -1,6 +1,9 @@
-import { data } from 'data';
+import { withPlaceholders } from '@utils/withPlaceholders';
+import { data as keyboardsData } from 'data';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const data = await Promise.all(withPlaceholders(keyboardsData, 60));
+
   res.status(200).json({ data });
 }
