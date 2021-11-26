920b16a02a5f6cb7e6aac8734115c3accd4facad
Chimson <crunor55@hotmail.com>
2021-11-26 18:51:54 +0100

    fix: add images to public folder

--- data.ts
+++ data.ts
@@ -807,8 +807,7 @@ export const data: Array<Product> = [
     image: {
       original:
         'https://cdn.shopify.com/s/files/1/0505/5912/3609/products/pdp_gallery_03_d0a9da7c-7102-477e-a401-f9cac8258a66_1200x@2x.png?v=1615353477',
-      thumbnail:
-        'https://cdn.shopify.com/s/files/1/0505/5912/3609/products/Vulcan-Pro-US_2x_4f123e32-2ae5-4a64-9b4b-dd8414da7441_550x534.png?v=1615353477',
+      thumbnail: '/assets/images/roccat_vulcan_pro.png',
     },
     description: ['Award Winning Design', '100 million keystroke life-cycle', '32-bit ARM Cortex-M0 based processor'],
   },
@@ -825,8 +824,7 @@ export const data: Array<Product> = [
     image: {
       original:
         'https://cdn.shopify.com/s/files/1/0505/5912/3609/products/pdp_gallery_03_d0a9da7c-7102-477e-a401-f9cac8258a66_1200x@2x.png?v=1615353477',
-      thumbnail:
-        'https://cdn.shopify.com/s/files/1/0508/6747/0494/products/Magma-US_2x_18e58b2d-0280-4a6b-b0c0-0f4e346fcde0_550x534.png?v=1619698239',
+      thumbnail: '/assets/images/roccat_magma.png',
     },
     description: ['Award Winning Design', '100 million keystroke life-cycle', '32-bit ARM Cortex-M0 based processor'],
   },
@@ -843,8 +841,7 @@ export const data: Array<Product> = [
     image: {
       original:
         'https://cdn.shopify.com/s/files/1/0505/5912/3609/products/pdp_gallery_03_d0a9da7c-7102-477e-a401-f9cac8258a66_1200x@2x.png?v=1615353477',
-      thumbnail:
-        'https://cdn.shopify.com/s/files/1/0508/6747/0494/products/Pyro-US_2x_c9595efa-fdb3-41ef-82cc-ddb4d4b6d754_550x534.png?v=1619698268',
+      thumbnail: '/assets/images/roccat_pyro.png',
     },
     description: ['Award Winning Design', '100 million keystroke life-cycle', '32-bit ARM Cortex-M0 based processor'],
   },
@@ -861,8 +858,7 @@ export const data: Array<Product> = [
     image: {
       original:
         'https://cdn.shopify.com/s/files/1/0505/5912/3609/products/pdp_gallery_03_d0a9da7c-7102-477e-a401-f9cac8258a66_1200x@2x.png?v=1615353477',
-      thumbnail:
-        'https://cdn.shopify.com/s/files/1/0508/6747/0494/products/Vulcan-120-US-2x-1_550x534.png?v=1605922046',
+      thumbnail: '/assets/images/roccat_vulcan_122_aimo.png',
     },
     description: ['Award Winning Design', '100 million keystroke life-cycle', '32-bit ARM Cortex-M0 based processor'],
   },
