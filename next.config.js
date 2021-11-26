5d18ea34baeae6904bec0f2a4cba5514f6e1dc64
Piotr Fidurski <crunor55@hotmail.com>
2021-11-26 20:00:19 +0100

    fix: remove placeholders from keycaps (#33)
    * fix: remove placeholders from keycaps
    
    * fix: comment cache headers temporarily
    
    * fix: return response from api route
    
    * fix: remove withPlaiceholder from next.config.js
    
    * fix: reduce keycaps array
    
    * fix: remove plaiceholder from config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-const { withPlaiceholder } = require('@plaiceholder/next');
 
-module.exports = withPlaiceholder({
+module.exports = {
   env: {
     BASE_URL: process.env.NODE_ENV === 'production' ? 'https://glowykeys.vercel.app' : 'http://localhost:3000',
   },
   images: {
     domains: ['cdn.shopify.com', 'spcgear.com'],
   },
-});
+};
--- pages/api/keyboards.ts
+++ pages/api/keyboards.ts
@@ -1,9 +1,6 @@
-import { withPlaceholders } from '@utils/withPlaceholders';
-import { data as keyboardsData } from 'data';
+import { data } from 'data';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const data = await Promise.all(withPlaceholders(keyboardsData, 60));
-
-  res.status(200).json({ data });
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({ data });
 }
