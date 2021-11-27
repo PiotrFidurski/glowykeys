fb47893b8f9798172b52bf2c305b274a940b51ce
Piotr Fidurski <crunor55@hotmail.com>
2021-11-27 10:45:29 +0100

    Switchespage (#34)
    * chore: remove plaiceholder related packages
    
    * fix: use the same image as placeholder in products
    
    * chore: delete hello api route
    
    * chore: update switches prices
    
    * feat: add switches api route
    
    * feat: build switches page
    
    * fix: fix typo in placeholder path
    
    * chore: update package-lock
    
    * chore: remove contrast and brightness filter from product image
    
    * refactor: remove withPlaceholders utility

---
+++ pages/api/switches.ts
@@ -0,0 +1,6 @@
+import { switchesData as data } from 'data';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({ data });
+}
