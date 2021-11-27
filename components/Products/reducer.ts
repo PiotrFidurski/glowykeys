9bf8fadc28d4e8539ee7efe71224734cfac25553
Chimson <crunor55@hotmail.com>
2021-11-27 19:24:33 +0100

    refactor: declare filterToBeUpdated var

--- components/Products/reducer.ts
+++ components/Products/reducer.ts
@@ -4,30 +4,36 @@ export function reducer(state: State, action: Action): State {
   switch (action.type) {
     case actionTypes.orderByPrice: {
       const order = action.payload as string;
+
       return {
         ...state,
         sort: { ...state.sort, order },
       };
     }
     case actionTypes.setFilter: {
-      const { value, type } = action.payload as { value: string; type: string };
+      const { filter, type } = action.payload as { filter: string; type: string };
 
-      const isSelected = state.activeFilters[type].includes(value);
+      const isSelected = state.activeFilters[type].includes(filter);
+
+      const filterToBeUpdated: Array<string> = state.activeFilters[type];
 
       return {
         ...state,
         activeFilters: isSelected
-          ? { ...state.activeFilters, [type]: state.activeFilters[type].filter((filter: string) => filter !== value) }
-          : { ...state.activeFilters, [type]: [...state.activeFilters[type], value] },
+          ? { ...state.activeFilters, [type]: filterToBeUpdated.filter((value: string) => value !== filter) }
+          : { ...state.activeFilters, [type]: [...filterToBeUpdated, filter] },
       };
     }
     case actionTypes.unsetFilter: {
       const { filter, type } = action.payload as { filter: string; type: string };
+
+      const filterToBeUpdated: Array<string> = state.activeFilters[type];
+
       return {
         ...state,
         activeFilters: {
           ...state.activeFilters,
-          [type]: state.activeFilters[type].filter((value: string) => value !== filter),
+          [type]: filterToBeUpdated.filter((value: string) => value !== filter),
         },
       };
     }
