50df1d3c7613c3470e2217963dea9a18e93c4010
Chimson <crunor55@hotmail.com>
2021-12-01 20:03:10 +0100

    test: use RouterContext.Provider in test Providers
    mock next router and pass it to provider, add third arg to custom render
    routerProps

--- lib/test-utils.tsx
+++ lib/test-utils.tsx
@@ -6,21 +6,54 @@ import { GlobalStyles } from '@styled/GlobalStyles';
 import { theme } from '@styled/theme';
 import { render, RenderOptions } from '@testing-library/react';
 import { cartData } from 'data';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import { NextRouter } from 'next/router';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-const Providers = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <CartProvider ui={<Cart />} reducer={reducer} initialItems={cartData}>
-      <GlobalStyles />
-      <Navbar />
-      {children}
-    </CartProvider>
-  </ThemeProvider>
+export const mockRouter = ({ query, ...props }: Partial<NextRouter> = {}): NextRouter => ({
+  asPath: '/',
+  basePath: '/',
+  back: jest.fn(() => Promise.resolve(true)),
+  beforePopState: jest.fn(() => Promise.resolve(true)),
+  events: {
+    on: jest.fn(),
+    off: jest.fn(),
+    emit: jest.fn(),
+  },
+  isFallback: false,
+  isLocaleDomain: true,
+  isPreview: true,
+  isReady: true,
+  pathname: '/',
+  push: jest.fn(() => Promise.resolve(true)),
+  prefetch: jest.fn(() => Promise.resolve()),
+  reload: jest.fn(() => Promise.resolve(true)),
+  replace: jest.fn(() => Promise.resolve(true)),
+  route: '/',
+  query: { ...query },
+  ...props,
+});
+
+interface ProviderProps {
+  children: React.ReactNode;
+  routerProps: Partial<NextRouter>;
+}
+
+const Providers = ({ children, routerProps }: ProviderProps) => (
+  <RouterContext.Provider value={mockRouter(routerProps)}>
+    <ThemeProvider theme={theme}>
+      <CartProvider ui={<Cart />} reducer={reducer} initialItems={cartData}>
+        <GlobalStyles />
+        <Navbar />
+        {children}
+      </CartProvider>
+    </ThemeProvider>
+  </RouterContext.Provider>
 );
 
-const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
-  render(ui, { wrapper: Providers, ...options });
+const customRender = (ui: React.ReactElement, options?: RenderOptions, routerProps?: Partial<NextRouter>) =>
+  render(ui, { wrapper: (props) => Providers({ routerProps, ...props }), ...options });
 
 export * from '@testing-library/react';
 export { customRender as render };
