1a58c918ad00d1b8bab0afe1322d8135ef68bf0a
Chimson <crunor55@hotmail.com>
2021-12-01 15:42:37 +0100

    refactor: remove react fragment wrapper

--- components/CartItem/CartItem.tsx
+++ components/CartItem/CartItem.tsx
@@ -22,69 +22,67 @@ function CartItem({ product }: Props) {
   } = useCart();
 
   return (
-    <>
-      <S.Article
-        id={`${product.name} article`}
-        aria-label={`${product.name} cart item`}
-        role="article"
-        as={motion.article}
-        layout
-      >
-        <S.Container>
-          <S.ImageWrapper>
-            <Image src={product.image.thumbnail} layout="fill" objectFit="contain" />
-          </S.ImageWrapper>
-          <S.DetailsContainer>
-            <S.DetailsHeaderWrapper>
-              <S.A href="/">{product.name}</S.A>
-              <RoundButton
-                style={{ minHeight: '30px', maxWidth: '30px', minWidth: '30px' }}
+    <S.Article
+      id={`${product.name} article`}
+      aria-label={`${product.name} cart item`}
+      role="article"
+      as={motion.article}
+      layout
+    >
+      <S.Container>
+        <S.ImageWrapper>
+          <Image src={product.image.thumbnail} layout="fill" objectFit="contain" />
+        </S.ImageWrapper>
+        <S.DetailsContainer>
+          <S.DetailsHeaderWrapper>
+            <S.A href="/">{product.name}</S.A>
+            <RoundButton
+              style={{ minHeight: '30px', maxWidth: '30px', minWidth: '30px' }}
+              type="button"
+              aria-label={`remove ${product.name} from cart`}
+              onClick={() => dispatch({ type: actionTypes.removeItem, payload: product })}
+            >
+              <Delete width="15" height="15" fill="white" />
+            </RoundButton>
+          </S.DetailsHeaderWrapper>
+          <S.DetailsContentWrapper>
+            <S.QuantityContainer>
+              <S.QuantityButton
                 type="button"
-                aria-label={`remove ${product.name} from cart`}
-                onClick={() => dispatch({ type: actionTypes.removeItem, payload: product })}
+                onClick={() => incrementQty({ dispatch, updates: { items, product } })}
+                aria-label={`add one more ${product.name} to cart`}
               >
-                <Delete width="15" height="15" fill="white" />
-              </RoundButton>
-            </S.DetailsHeaderWrapper>
-            <S.DetailsContentWrapper>
-              <S.QuantityContainer>
-                <S.QuantityButton
-                  type="button"
-                  onClick={() => incrementQty({ dispatch, updates: { items, product } })}
-                  aria-label={`add one more ${product.name} to cart`}
-                >
-                  <Plus width="15" height="15" fill="white" />
-                </S.QuantityButton>
-                <S.QTY tabIndex={-1} aria-label="Quantity" inputMode="numeric" readOnly value={product.qty} />
-                <S.QuantityButton
-                  reverseBorderRadius
-                  transformOn={product.qty === 0}
-                  type="button"
-                  onClick={() => {
-                    if (product.qty > 0) {
-                      decrementQty({ dispatch, updates: { items, product } });
-                    } else {
-                      dispatch({ type: actionTypes.removeItem, payload: product });
-                    }
-                  }}
-                  aria-label={
-                    product.qty > 0 ? `remove one ${product.name} from cart` : `remove ${product.name} from cart`
+                <Plus width="15" height="15" fill="white" />
+              </S.QuantityButton>
+              <S.QTY tabIndex={-1} aria-label="Quantity" inputMode="numeric" readOnly value={product.qty} />
+              <S.QuantityButton
+                reverseBorderRadius
+                transformOn={product.qty === 0}
+                type="button"
+                onClick={() => {
+                  if (product.qty > 0) {
+                    decrementQty({ dispatch, updates: { items, product } });
+                  } else {
+                    dispatch({ type: actionTypes.removeItem, payload: product });
                   }
-                >
-                  {product.qty === 0 ? (
-                    <Delete width="15" height="15" fill="white" />
-                  ) : (
-                    <Minus width="15" height="15" fill="white" />
-                  )}
-                </S.QuantityButton>
-              </S.QuantityContainer>
-              <S.Paragraph>{(product.price * product.qty).toFixed(2)}$</S.Paragraph>
-            </S.DetailsContentWrapper>
-          </S.DetailsContainer>
-        </S.Container>
-      </S.Article>
+                }}
+                aria-label={
+                  product.qty > 0 ? `remove one ${product.name} from cart` : `remove ${product.name} from cart`
+                }
+              >
+                {product.qty === 0 ? (
+                  <Delete width="15" height="15" fill="white" />
+                ) : (
+                  <Minus width="15" height="15" fill="white" />
+                )}
+              </S.QuantityButton>
+            </S.QuantityContainer>
+            <S.Paragraph>{(product.price * product.qty).toFixed(2)}$</S.Paragraph>
+          </S.DetailsContentWrapper>
+        </S.DetailsContainer>
+      </S.Container>
       <S.Hr />
-    </>
+    </S.Article>
   );
 }
 
