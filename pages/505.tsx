b976494292c72ba68d03c043900c640b8d4b75b8
Piotr Fidurski <crunor55@hotmail.com>
2021-12-02 16:18:31 +0100

    feat: add 500 custom page

---
+++ pages/505.tsx
@@ -0,0 +1,13 @@
+import { Container, H1, Paragraph } from '@styled/pages/404PageStyles';
+import * as React from 'react';
+
+function Custom500() {
+  return (
+    <Container>
+      <H1>500</H1>
+      <Paragraph>Looks like something went really wrong</Paragraph>
+    </Container>
+  );
+}
+
+export default Custom500;
