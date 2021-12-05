8d31db7db9d7f3cfd83a24aa745e61350c07a1e6
Piotr Fidurski <crunor55@hotmail.com>
2021-12-05 12:55:50 +0100

    chore: add dbConnect utility to lib/ folder

---
+++ lib/dbConnect.ts
@@ -0,0 +1,48 @@
+import { connect } from 'mongoose';
+
+const username = process.env.DB_USER;
+const password = process.env.DB_PWD;
+
+const MONGODB_URI =
+  process.env.NODE_ENV === 'development' ? `mongodb://${username}:${password}@localhost:27017/glowykeys` : ``;
+
+if (!username || password) {
+  throw new Error('Please make sure to define DB_USER and DB_PWD environment variables inside .env.local');
+}
+
+interface CachedConnection {
+  connection: typeof import('mongoose');
+  promise: Promise<typeof import('mongoose')>;
+}
+
+let cached: CachedConnection = global.mongoose;
+
+if (!cached) {
+  // eslint-disable-next-line no-multi-assign
+  cached = global.mongoose = { connection: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.connection) {
+    return cached.connection;
+  }
+
+  if (!cached.promise) {
+    const config = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+      useFindAndModify: true,
+      useCreateIndex: true,
+    };
+
+    cached.promise = connect(MONGODB_URI, config).then((mongoose) => mongoose);
+  }
+
+  cached.connection = await cached.promise;
+
+  return cached.connection;
+}
+
+export default dbConnect;
