8d03d33e21fff68fcbc9caa3bca216e8c3d15276
Piotr Fidurski <crunor55@hotmail.com>
2021-12-05 15:56:10 +0100

    refactor: return data from mongodb instead of data.ts file

--- pages/api/keyboards.ts
+++ pages/api/keyboards.ts
@@ -1,12 +1,24 @@
 import dbConnect from '@utils/dbConnect';
-import { ApiResponse } from '@utils/types';
+import { ApiErrorResponse, ApiResponse } from '@utils/types';
 import Product, { ProductDocument } from 'models/Product';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResponse<ProductDocument>>) {
-  await dbConnect();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse<ProductDocument> | ApiErrorResponse>
+) {
+  try {
+    await dbConnect();
 
-  const docs: Array<ProductDocument> = await Product.aggregate([{ $match: { type: { $eq: 'keyboard' } } }]);
+    const docs: Array<ProductDocument> = await Product.aggregate([
+      { $match: { type: { $eq: 'keyboard' } } },
+      { $project: { description: 0 } },
+      { $addFields: { id: '$_id' } },
+    ]);
 
-  return res.status(200).json({ data: docs });
+    return res.status(200).json({ data: docs });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: error.message });
+  }
 }
--- pages/api/switches.ts
+++ pages/api/switches.ts
@@ -1,6 +1,24 @@
-import { switchesData as data } from 'data';
+import dbConnect from '@utils/dbConnect';
+import { ApiErrorResponse, ApiResponse } from '@utils/types';
+import Product, { ProductDocument } from 'models/Product';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ data });
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse<ProductDocument> | ApiErrorResponse>
+) {
+  try {
+    await dbConnect();
+
+    const docs = await Product.aggregate([
+      { $match: { type: { $eq: 'switch' } } },
+      { $project: { description: 0 } },
+      { $addFields: { id: '$_id' } },
+    ]);
+
+    return res.status(200).json({ data: docs });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: error.message });
+  }
 }
