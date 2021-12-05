28594763d9350ecd6e17496ccfe76831391f650f
Piotr Fidurski <crunor55@hotmail.com>
2021-12-05 13:40:10 +0100

    refactor: get data from database instead of data.ts

--- pages/api/keyboards.ts
+++ pages/api/keyboards.ts
@@ -1,6 +1,12 @@
-import { keyboardsData as data } from 'data';
+import dbConnect from '@utils/dbConnect';
+import { ApiResponse } from '@utils/types';
+import Product, { ProductDocument } from 'models/Product';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ data });
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResponse<ProductDocument>>) {
+  await dbConnect();
+
+  const docs: Array<ProductDocument> = await Product.aggregate([{ $match: { type: { $eq: 'keyboard' } } }]);
+
+  return res.status(200).json({ data: docs });
 }
