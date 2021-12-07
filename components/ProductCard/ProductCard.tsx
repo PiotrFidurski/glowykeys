d00ce35fdf35fa11b2745b2727bc9ae250199240
Piotr Fidurski <crunor55@hotmail.com>
2021-12-07 19:32:08 +0100

    feat: wrap card image in next/link

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -4,28 +4,36 @@ import { SquareButton } from '@utils/style-utils';
 import { Product as ProductType } from '@utils/types';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import * as React from 'react';
-import { Article, H2, Heading, Paragraph, ProductImageContainer } from './styles';
+import { A, Article, H2, Heading, Paragraph, ProductImageContainer } from './styles';
 
 interface Props {
   product: ProductType & { image: { original: string; thumbnail: string; placeholder?: string } };
 }
 
-function Product({ product }: Props) {
+function ProductCard({ product }: Props) {
   const { dispatch } = useCart();
+
+  const { pathname } = useRouter();
 
   return (
     <Article as={motion.article} layout role="article" aria-label={product.name}>
-      <ProductImageContainer>
-        <Image
-          src={product.image.thumbnail}
-          loading="lazy"
-          blurDataURL={product.image.placeholder}
-          placeholder="blur"
-          objectFit="contain"
-          layout="fill"
-        />
-      </ProductImageContainer>
+      <Link href={`${pathname}/${product.id}`} prefetch={false} passHref>
+        <A>
+          <ProductImageContainer>
+            <Image
+              src={product.image.thumbnail}
+              loading="lazy"
+              blurDataURL={product.image.placeholder}
+              placeholder="blur"
+              objectFit="contain"
+              layout="fill"
+            />
+          </ProductImageContainer>
+        </A>
+      </Link>
       <Heading>
         <H2>{product.name}</H2>
         <Paragraph>{product.price.toFixed(2)}$</Paragraph>
@@ -41,4 +49,4 @@ function Product({ product }: Props) {
   );
 }
 
-export default Product;
+export default ProductCard;
