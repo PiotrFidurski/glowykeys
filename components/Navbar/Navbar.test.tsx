cb19e6252c6e2c5908c6ec03b6eff66e9ebcfc22
Piotr Fidurski <crunor55@hotmail.com>
2021-12-13 21:02:54 +0100

    test: remove redundant mocks from tests

--- components/Navbar/Navbar.test.tsx
+++ components/Navbar/Navbar.test.tsx
@@ -3,14 +3,6 @@ import { screen } from '@testing-library/react';
 import { render } from '@utils/test-utils';
 import { testData } from 'data';
 
-jest.mock('next/link', () => ({ children, href }: React.PropsWithChildren<{ href: string }>) => {
-  const React = require('react');
-
-  const clonedChildren = React.cloneElement(React.Children.only(children), { href });
-
-  return clonedChildren;
-});
-
 test('clicking on logo redirects back to HomePage', () => {
   render(<KeyboardsPage keyboards={testData} />);
 
@@ -25,6 +17,4 @@ test('clicking on sign in link links to sign in page', () => {
   const signInLink = screen.getByText(/Sign in/i);
 
   expect(signInLink).toHaveAttribute('href', '/signin');
-
-  jest.restoreAllMocks();
 });
