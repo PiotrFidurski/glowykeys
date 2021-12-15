66a3aa7f36725ec6fadd2b372dd5ff094535f401
Piotr Fidurski <crunor55@hotmail.com>
2021-12-15 20:48:20 +0100

    test: extract search test to its own file

--- cypress/e2e/ui/HomePage.test.ts
+++ cypress/e2e/ui/HomePage.test.ts
@@ -61,22 +61,6 @@ describe('HomePage tests', () => {
     cy.findByRole('button', { name: /casual filter/ });
   });
 
-  it('each search result is a link that redirects to details page', () => {
-    cy.intercept({ url: '/api/search?query=a', query: { query: 'a' } }, { fixture: 'search.json' }).as('getResults');
-
-    cy.visit('/');
-
-    cy.findByRole('button', { name: /open search dialog/i }).click();
-
-    cy.findByRole('textbox', { name: /search/i }).type('a');
-
-    cy.wait('@getResults');
-
-    cy.findByRole('link', { name: /gigabyte force k81/i }).click();
-
-    cy.url().should('eq', `${Cypress.config().baseUrl}/keyboards/gigabyte-force-k81`);
-  });
-
   it('has a functioning hamburger menu on smaller viewports', () => {
     cy.viewport('iphone-5');
 
