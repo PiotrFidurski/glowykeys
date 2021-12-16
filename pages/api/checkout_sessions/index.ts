48b3761ac673b0a4159f0c1f4bfa22f4d2b7b9f9
Piotr Fidurski <crunor55@hotmail.com>
2021-12-16 21:41:49 +0100

    chore: add checkout_sessions api route
    adds checkout api route that uses stripe webhook

---
+++ pages/api/checkout_sessions/index.ts
@@ -0,0 +1,27 @@
+import { transfromItemsToStrpe } from '@utils/tranformItemsToStripe';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIKE_SECRET_KEY, { apiVersion: '2020-08-27' });
+
+const redirectUrl = process.env.BASE_URL;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { items } = req.body;
+
+    const transformedItems = transfromItemsToStrpe(items);
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: transformedItems,
+      mode: 'payment',
+      success_url: `${redirectUrl}?status=success`,
+      cancel_url: `${redirectUrl}?status=cancel`,
+    });
+
+    return res.json({ sessionId: session.id });
+  } catch (error) {
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
+}
