524641959ba2b1d6c75ec28ebd862baf9378358e
Piotr Fidurski <crunor55@hotmail.com>
2021-12-18 10:09:28 +0100

    refactor: fix typo in command description

--- cypress/types/index.d.ts
+++ cypress/types/index.d.ts
@@ -3,7 +3,7 @@
 declare namespace Cypress {
   interface Chainable {
     /**
-     * Custom command to intercept a request and search
+     * Custom command to mock data returned from server
      * @params {string} path - string representing api route to mock e.g /api/keyboards
      * @params {string} fixture - string representing json file in fixtures folder e.g 'keycaps.json'
      * @example cy.mockServerData({path:"/api/keyboards", fixture: 'keyboards.json'})
