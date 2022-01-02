ee66f2996add462655706fe7971eaf26ec74afc3
Piotr Fidurski <crunor55@hotmail.com>
2022-01-02 11:20:08 +0100

    fix: call callback from createCheckoutSession

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -31,14 +31,10 @@ function Cart() {
 
   const { priceInDollars } = useFormatCurrency(subtotal);
 
-  React.useEffect(() => {
-    setLoading(false);
-  }, []);
-
   const handleClick = async () => {
     if (items.length) {
       setLoading(true);
-      await createCheckoutSession(items);
+      createCheckoutSession({ items, callback: () => setLoading(false) });
     } else {
       dispatch({ type: actionTypes.closeMenu });
     }
